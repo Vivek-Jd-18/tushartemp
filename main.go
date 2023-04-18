a22b77141c24d2be1fe75c350ad4337ba926242d
VivekJd-18 <vivekdhage.vhits@gmail.com>
2023-04-18 16:25:25 +0530

    asaas

--- main.go
+++ main.go
@@ -2,9 +2,19 @@ package main
 
 import "fmt"
 
+
+func temp() {
+	fmt.Println("Hello World1")
+	fmt.Println("Hello World2")
+	fmt.Println("Hello World3")
+	fmt.Println("Hello World4")
+}
+
+
 func main() {
 	fmt.Println("Hello World1")
 	fmt.Println("Hello World2")
 	fmt.Println("Hello World3")
 	fmt.Println("Hello World4")
+	temp()
 }
